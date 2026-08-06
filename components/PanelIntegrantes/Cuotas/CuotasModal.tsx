342153ab345cf08304f0f34ab6e7082dc575461d
brianSalguero <briansalguero200217@gmail.com>
2026-08-06 15:36:28 +0200

    Panel Integrantes 06/08 2

--- components/PanelIntegrantes/Cuotas/CuotasModal.tsx
+++ components/PanelIntegrantes/Cuotas/CuotasModal.tsx
@@ -101,7 +101,7 @@ export default function CuotasModal({
         anios.push(a);
     }
     return (
-        <div className="fixed inset-0 z-50 bg-black/60 flex items-center justify-center p-4">
+        <div className="fixed inset-0 z-[9999] bg-black/60 flex items-center justify-center p-4">
 
             <div className="w-full max-w-2xl rounded-3xl bg-white dark:bg-slate-900 shadow-2xl">
 
